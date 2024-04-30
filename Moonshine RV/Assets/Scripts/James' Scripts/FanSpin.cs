19b8192efb9a8fb5be70775674b895fef7dcebd4
jamesrone <32492411+jaamesrone@users.noreply.github.com>
2024-04-30 15:42:17 -0700

    d

--- Moonshine RV/Assets/Scripts/James' Scripts/FanSpin.cs
+++ Moonshine RV/Assets/Scripts/James' Scripts/FanSpin.cs
@@ -3,14 +3,7 @@ using UnityEngine;
 public class FanSpin : MonoBehaviour
 {
     public GameObject Fan;
-    public float rotationSpeed = 100.0f; // Speed of rotation in degrees per second
-    private Vector3 originalScale;
-
-    void Start()
-    {
-        // save the original scale of the fan
-        originalScale = Fan.transform.localScale;
-    }
+    public float rotationSpeed = 100.0f; // speed of rotation in degrees per second
 
     void Update()
     {
